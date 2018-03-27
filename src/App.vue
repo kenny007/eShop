3300a6c6047416d52563ece4a8962a1ae1dde261
Kehinde Adebayo <press.spark@gmail.com>
2018-03-28 01:26:08 +0300

    Cart/Checkout page feature added

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,90 @@
 <template>
   <div id="app">
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">e-shop</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <router-link class="nav-link" to="/">Home</router-link>
-      </li>
-      <li class="nav-item">
-     <router-link class="nav-link"  to="/admin">Admin</router-link>
-       </li>
-      <li class="nav-item">
-       <router-link  class="nav-link" to="/cart">Cart</router-link>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+    <nav>
+      <div class="container">
+        <ul class="nav__left">
+          <li><router-link to="/"><i class="fa fa-home"></i> Home</router-link></li>
+          <li><router-link to="/admin"><i class="fa fa-user"></i> Admin</router-link></li>
+        </ul>
+        <ul class="nav__right">
+          <li><router-link to="/cart"><i class="fa fa-shopping-cart"></i> Cart ({{cartItemsCount}})</router-link></li>
+        </ul>
+      </div>
+    </nav>
+
+     <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow fixed-top">
+      <h5 class="my-0 mr-md-auto font-weight-normal">Company name</h5>
+      <nav class="my-2 my-md-0 mr-md-3">
+        <router-link class="p-2 text-dark" to="/"><i class="fa fa-home"></i> Home</router-link>
+        <router-link class="p-2 text-dark" to="/admin"><i class="fa fa-user"></i> Admin</router-link>
+        <router-link class="p-2 text-dark" to="/"><i class="fa fa-home"></i>Support</router-link>
+        <router-link class="p-2 text-dark" to="/"><i class="fa fa-user"></i> Pricing</router-link>
+        <router-link class="p-2 text-dark" to="/cart"><i class="fa fa-card"></i> Cart ({{cartItemsCount}})</router-link>
+      </nav>
+      <a class="btn btn-outline-primary" href="#">Sign up</a>
+    </div>
+
+
+
+
+
+    <router-view></router-view>
+    <div class="overlay" v-show="showLoader">
+      <div class="loading-spinner">
+        <div class="dot dotOne"></div>
+        <div class="dot dotTwo"></div>
+        <div class="dot dotThree"></div>
+      </div>
+    </div>
   </div>
-  
-</nav>
+</template>
 
+<script>
+import toastr from 'toastr'
+
+import {
+  ADD_PRODUCT_SUCCESS,
+  UPDATE_PRODUCT_SUCCESS,
+  REMOVE_PRODUCT_SUCCESS
+} from './store/mutation-types'
+export default {
+  name: 'app',
+  data () {
+    return {
+      cartItems: this.$store.state.cart
+    }
+  },
+  created () {
+    // Subscriptions for mutation
+    this.$store.subscribe((mutation) => {
+      if (mutation.payload) {
+        switch (mutation.type) {
+          case ADD_PRODUCT_SUCCESS:
+            toastr.success('Product created.', 'Success!')
+            break
+          case UPDATE_PRODUCT_SUCCESS:
+            toastr.success('Product updated.', 'Success!')
+            break
+          case REMOVE_PRODUCT_SUCCESS:
+            toastr.warning('Product deleted.', 'Deleted!')
+            break
+        }
+      }
+    })
+  },
+  computed: {
+    cartItemsCount () {
+      // Cart count
+      return this.cartItems.length
+    },
+    showLoader () {
+      // Loading spinner
+      return this.$store.state.showLoader
+    }
+  }
+}
+</script>
     <!-- Route Outlet -->
     <router-view>
 
@@ -52,5 +111,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+@import url('http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css');
+.navbar-toggle {
+     margin-left: 0px;
+    border-color: transparent;
+    padding: 2px;
+    border: 1px none transparent;
+}
+.nav-toggle-left {
+    float: left;
+    margin-left: 15px;
 }
 </style>
